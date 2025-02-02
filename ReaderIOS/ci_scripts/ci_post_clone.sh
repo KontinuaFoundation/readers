d09e9d7efaf2e8ffd7a6b00ce227fba74b94c00a
JonasSchiessl <j.schiessl2001@gmail.com>
2025-02-01 17:45:46 -0800

    trying different approach to get swftlint to work with Xcode Could

---
+++ ReaderIOS/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,14 @@
+#
+//  ci_post_clone.sh
+//  ReaderIOS
+//
+//  Created by Jonas Schiessl on 2/1/25.
+//
+
+#!/bin/sh
+
+# Print start of setup
+echo "Starting CI setup..."
+
+# Install SwiftLint using Homebrew
+brew install swiftlint
