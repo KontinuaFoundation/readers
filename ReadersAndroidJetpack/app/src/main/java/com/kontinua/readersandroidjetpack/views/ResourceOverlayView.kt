cfdabc25c738611a64c8606bd09647afb564c565
mollysandler <74861421+mollysandler@users.noreply.github.com>
2025-04-29 09:48:24 -0700

    comments removed

--- ReadersAndroidJetpack/app/src/main/java/com/kontinua/readersandroidjetpack/views/ResourceOverlayView.kt
+++ ReadersAndroidJetpack/app/src/main/java/com/kontinua/readersandroidjetpack/views/ResourceOverlayView.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
