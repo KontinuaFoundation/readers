53c8ba2195422aeaef8b34447f68621809f238d9
Devin Hadley <68879608+devinhadley@users.noreply.github.com>
2025-05-06 15:07:43 -0700

    cleanup

--- backend/readers_backend/readers_backend/views.py
+++ backend/readers_backend/readers_backend/views.py
@@ -3,6 +3,10 @@ from core.models import Collection
 
 
 class IndexView(TemplateView):
+    '''
+    Renders a grid of the latest en-US collection's workbooks.
+    '''
+
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
