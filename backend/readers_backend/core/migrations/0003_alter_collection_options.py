c24ac20e9ae364ff2664bda1ec4b76f70a07f5c3
Devin Hadley <68879608+devinhadley@users.noreply.github.com>
2025-05-12 10:11:18 -0700

    format

--- backend/readers_backend/core/migrations/0003_alter_collection_options.py
+++ backend/readers_backend/core/migrations/0003_alter_collection_options.py
@@ -5,12 +5,12 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
     dependencies = [
-        ('core', '0002_alter_collection_localization'),
+        ("core", "0002_alter_collection_localization"),
     ]
 
     operations = [
         migrations.AlterModelOptions(
-            name='collection',
-            options={'ordering': ['id']},
+            name="collection",
+            options={"ordering": ["id"]},
         ),
     ]
