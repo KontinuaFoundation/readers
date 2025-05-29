d2d83ab3bc61bd8071438cc2d52abc9a26912c9d
Luis Rodriguez <lrod011703@yahoo.com>
2025-05-28 18:31:20 -0700

    Update AnnotationManager.kt

--- ReadersAndroidJetpack/app/src/main/java/com/kontinua/readersandroidjetpack/util/AnnotationManager.kt
+++ ReadersAndroidJetpack/app/src/main/java/com/kontinua/readersandroidjetpack/util/AnnotationManager.kt
@@ -55,7 +55,7 @@ class AnnotationManager {
         toggleScribble(boolean)
     }
 
-    fun toggleText(boolean: Boolean){
+    fun toggleText(boolean: Boolean) {
         mode = AnnotationMode.TEXT
         toggleScribble(boolean)
     }
