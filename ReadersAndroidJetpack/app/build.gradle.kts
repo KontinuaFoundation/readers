a0ddfdf72d739fe48dc72e353385590ea0a28fe1
Pranav Gundu <pranav@gundu.me>
2026-05-27 15:47:31 -0400

    fix(android): restore lifecycle-viewmodel-compose dependency

--- ReadersAndroidJetpack/app/build.gradle.kts
+++ ReadersAndroidJetpack/app/build.gradle.kts
@@ -85,4 +85,5 @@ dependencies {
     // for saving bookmarks
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
